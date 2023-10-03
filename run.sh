ba3f2bf8ba75fc362e16335500b94cd6726b1784
SulRash <sultan.m.rashed@gmail.com>
2023-10-03 14:02:22 +0000

    Made the bash script prettier

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 BATCH_SIZE=128
-DATASET="cifar"
-CHEATSHEET=0
-EXP_NAME="NoCheatsheetResnet18-88x80-NoBlackColumn-Batch128pergpu"
 
+MODEL="vit_base_patch8_224"
+DATASET="cifar"
+
+CHEATSHEET=0
+CS_SIZE=16
+
+EXP_NAME="NoCheatsheet-NoBlackColumn-vit_base_patch8_224-160x160-Batch128pergpu"
 # EXP_NAME="DEBUGGING"
 
-deepspeed --num_gpus 8 src/main.py --dataset ${DATASET} --exp_name ${EXP_NAME} --batch_size ${BATCH_SIZE} --deepspeed_config "src/conf/ds_config.json" --cs_size 8
+if [ $CHEATSHEET == 1 ]; then
+    deepspeed --num_gpus 8 src/main.py --model ${MODEL} --dataset ${DATASET} --exp_name ${EXP_NAME} --batch_size ${BATCH_SIZE} --cheatsheet --cs_size ${CS_SIZE} --deepspeed_config "src/conf/ds_config.json"
+else
+    deepspeed --num_gpus 8 src/main.py --model ${MODEL} --dataset ${DATASET} --exp_name ${EXP_NAME} --batch_size ${BATCH_SIZE} --cs_size ${CS_SIZE} --deepspeed_config "src/conf/ds_config.json"
+fi
