392d3b644169daf6df1c5d934ca731ddf0275e78
Sultan <92447824+SulRash@users.noreply.github.com>
2023-11-15 00:35:03 +0300

    Added ability to disable wandb

--- src/train.py
+++ src/train.py
@@ -20,20 +20,21 @@ def main(args):
         deepspeed.init_distributed()
 
     if dist.get_rank() == 0:
-        run = wandb.init(
-            name=args.exp_name,
-            project='Cheatsheet',
-            notes=args.exp_name,
-            config=vars(args)
-        )
+        if args.wandb:
+            run = wandb.init(
+                name=args.exp_name,
+                project='Cheatsheet',
+                notes=args.exp_name,
+                config=vars(args)
+            )
 
-        wandb.define_metric("epoch")
-        wandb.define_metric("train/*", step_metric="epoch")
-        wandb.define_metric("test/*", step_metric="epoch")
+            wandb.define_metric("epoch")
+            wandb.define_metric("train/*", step_metric="epoch")
+            wandb.define_metric("test/*", step_metric="epoch")
 
-        example_position, example_class = get_examples(train_data)
+            example_position, example_class = get_examples(train_data)
 
-        wandb.log({"example_positions": example_position, "example_classes": example_class})
+            wandb.log({"example_positions": example_position, "example_classes": example_class})
 
     test_dataloader = get_dataloader(test_data, args.batch_size)
 
@@ -57,23 +58,24 @@ def main(args):
                 test_results, test_pos_chart, test_cls_chart = test(model, test_dataloader, epoch, args.exp_name, args.dataset, "test")
                 train_results, train_pos_chart, train_cls_chart = test(model, train_dataloader, epoch, args.exp_name, args.dataset, "train")
 
-                train_pos_acc = train_results["train"]["Positional Accuracy"]["Total Positional Accuracy"]
-                test_pos_acc = test_results["test"]["Positional Accuracy"]["Total Positional Accuracy"]
-                train_class_acc = train_results["train"]["Class Accuracy"]["Total Class Accuracy"]
-                test_class_acc = test_results["test"]["Class Accuracy"]["Total Class Accuracy"]
+                if args.wandb:
+                    train_pos_acc = train_results["train"]["Positional Accuracy"]["Total Positional Accuracy"]
+                    test_pos_acc = test_results["test"]["Positional Accuracy"]["Total Positional Accuracy"]
+                    train_class_acc = train_results["train"]["Class Accuracy"]["Total Class Accuracy"]
+                    test_class_acc = test_results["test"]["Class Accuracy"]["Total Class Accuracy"]
 
-                #train_acc_per_pos = train_results["train"]["Positional Accuracy"]["Accuracy Per Position"]
-                #test_acc_per_pos = test_results["test"]["Positional Accuracy"]["Accuracy Per Position"]
-                #train_acc_per_class = train_results["train"]["Class Accuracy"]["Accuracy Per Class"]
-                #test_acc_per_class = test_results["test"]["Class Accuracy"]["Accuracy Per Class"]
+                    #train_acc_per_pos = train_results["train"]["Positional Accuracy"]["Accuracy Per Position"]
+                    #test_acc_per_pos = test_results["test"]["Positional Accuracy"]["Accuracy Per Position"]
+                    #train_acc_per_class = train_results["train"]["Class Accuracy"]["Accuracy Per Class"]
+                    #test_acc_per_class = test_results["test"]["Class Accuracy"]["Accuracy Per Class"]
 
-                wandb.log({
-                    "epoch": epoch, 
-                    "train/total_pos_acc": train_pos_acc, "test/total_acc": test_pos_acc,
-                    "train/total_class_acc": train_class_acc, "test/total_class_acc": test_class_acc,
-                    "train/acc_per_pos": train_pos_chart, "test/acc_per_pos": test_pos_chart,
-                    "train/acc_per_class": train_cls_chart, "test/acc_per_class": test_cls_chart
-                    })
+                    wandb.log({
+                        "epoch": epoch, 
+                        "train/total_pos_acc": train_pos_acc, "test/total_acc": test_pos_acc,
+                        "train/total_class_acc": train_class_acc, "test/total_class_acc": test_class_acc,
+                        "train/acc_per_pos": train_pos_chart, "test/acc_per_pos": test_pos_chart,
+                        "train/acc_per_class": train_cls_chart, "test/acc_per_class": test_cls_chart
+                        })
 
         dist.barrier()
 
--- src/utils/arguments.py
+++ src/utils/arguments.py
@@ -14,11 +14,39 @@ def get_train_args():
 
     parser.add_argument('--model', type=str, choices=['resnet18', 'resnet34', 'flexivit_base', 'vit_large_patch14_224', 'vit_huge_patch14_224', 'vit_base_patch8_224', 'vit_small_patch8_224', 'deit3_base_patch16_384_in21ft1k'])
 
+    parser.add_argument('--wandb', action="store_true")
     parser.add_argument('--exp_name', type=str, default='Default')
     parser.add_argument('--test_interval', type=int, default=5)
 
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--train_epochs', type=int, default=250)
+    
+    parser.add_argument('--load_dir', type=str, default=None)
+    parser.add_argument('--ckpt_id', default=None)
+    parser.add_argument('--save_interval', type=int, default=10)
+
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--local_rank', type=int, default=-1)
+
+    parser = deepspeed.add_config_arguments(parser)
+    return parser.parse_args()
+
+def get_test_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cheatsheet', action="store_true")
+    parser.add_argument('--randomize_sheet', action="store_true")
+    parser.add_argument('--one_image', action="store_true")
+    parser.add_argument('--one_image_per_class', action="store_true")
+    parser.add_argument('--img_per_class', default=0)
+    parser.add_argument('--cs_size', type=int, default=8)
+    parser.add_argument('--dataset', type=str, choices=['cifar10', 'cifar100', 'mnist'], default='cifar10')
+
+    parser.add_argument('--model', type=str, choices=['resnet18', 'resnet34', 'flexivit_base', 'vit_large_patch14_224', 'vit_huge_patch14_224', 'vit_base_patch8_224', 'vit_small_patch8_224', 'deit3_base_patch16_384_in21ft1k'])
+
+    parser.add_argument('--exp_name', type=str, default='Default')
+    parser.add_argument('--test_interval', type=int, default=5)
+
+    parser.add_argument('--batch_size', type=int, default=16)
     
     parser.add_argument('--load_dir', type=str, default=None)
     parser.add_argument('--ckpt_id', default=None)
