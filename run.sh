2bbba52fc586287bf61e385486cd0f61c5ccfe02
Sultan <92447824+SulRash@users.noreply.github.com>
2023-11-15 14:08:48 +0300

    Made run copy ds config

--- run.sh
+++ run.sh
@@ -1,20 +1,23 @@
 NUM_GPUS=8
-BATCH_SIZE=64
+BATCH_SIZE=512
 
 let GLOBAL_BATCH=${NUM_GPUS}*${BATCH_SIZE}
 
 #MODEL="vit_small_patch8_224"
 #MODEL="resnet18"
 #MODEL="deit3_base_patch16_384_in21ft1k"
-#MODEL="vit_base_patch8_224"
-MODEL="vit_large_patch14_224"
+MODEL="vit_base_patch8_224"
+#MODEL="vit_large_patch14_224"
 DATASET="cifar10"
 IMG_PER_CLASS=0
 CHEATSHEET=1
 CS_SIZE=14
 
-
 EXP_NAME="1CYC-Cheatsheet-${DATASET}-${MODEL}-CS_SIZE${CS_SIZE}-GlobalBatch${GLOBAL_BATCH}-RandomizeSheet-ImgPerClass${IMG_PER_CLASS}"
+
+mkdir experiments/${EXP_NAME} -p
+cp src/conf/ds_config.json experiments/${EXP_NAME}/ds_config.json
+
 deepspeed --num_gpus ${NUM_GPUS} src/train.py \
     --model ${MODEL} \
     --dataset ${DATASET} \
