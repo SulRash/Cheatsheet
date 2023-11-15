c8842be8d77b700e964e8fe6e11077bc21760d84
Sultan <92447824+SulRash@users.noreply.github.com>
2023-11-15 13:26:56 +0300

    Added new cheatsheet method, concatenation!

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,13 +4,16 @@ import timm
 
 from models.resnet import ResNet18, ResNet34
 
-def get_model(model_name: str, num_classes: int = 10, cs_size: int = 8):
+def get_model(model_name: str, num_classes: int = 10, cs_size: int = 8, concat: bool = False):
 
     max_images_in_row = 10
-    
-    new_image_box = cs_size * max_images_in_row
-    additional_rows = cs_size * ceil(int(num_classes)/max_images_in_row)
-    new_image_height = cs_size * max_images_in_row + additional_rows
+    if concat:
+        new_image_box = 32*11
+        new_image_height = 32
+    else:
+        new_image_box = cs_size * max_images_in_row
+        additional_rows = cs_size * ceil(int(num_classes)/max_images_in_row)
+        new_image_height = cs_size * max_images_in_row + additional_rows
     
     if "resnet" in model_name:
         return timm.create_model(
--- src/transforms/addcheatsheet.py
+++ src/transforms/addcheatsheet.py
@@ -1,7 +1,6 @@
 from math import ceil
 from typing import Dict
 from random import randint, shuffle
-from copy import copy
 
 from PIL import Image
 
@@ -16,21 +15,17 @@ class AddCheatsheet():
         self.randomize_sheet = args.randomize_sheet
         self.one_image = args.one_image
         self.one_image_per_class = args.one_image_per_class
+        self.concat = args.concat
 
     def __call__(self, img: Image, target: int):
 
         sheet = self.sheet
-        max_images_in_row = 10
 
         if self.one_image:
             img = sheet[0]
             target = 0
         elif self.one_image_per_class:
             img = sheet[target]
-
-        new_image_box = self.cs_size * max_images_in_row
-        additional_rows = self.cs_size * ceil(int(self.num_classes)/max_images_in_row)
-        new_image_height = self.cs_size * max_images_in_row + additional_rows
         
         # Adds rows on the top for the cheatsheet
         if self.cheatsheet:
@@ -38,14 +33,29 @@ class AddCheatsheet():
             if self.randomize_sheet:
                 sheet, target = self.change_reference(target)
 
-            modified = self.add_cheatsheet_rows(
-                img=img,
-                sheet=sheet,
-                max_images_in_row=max_images_in_row,
-                new_image_box=new_image_box,
-                new_image_height=new_image_height,
-                additional_rows=additional_rows
-            )
+            if self.concat:
+                new_image_width = img.size[0] * (self.num_classes+1)
+                modified = modified = Image.new('RGB', (new_image_width, img.size[1]))
+                modified.paste(img, (0,0))
+                for i in range(self.num_classes):
+                    x_loc = img.size[0] * (i+1)
+                    modified.paste(sheet[i], (x_loc,0))
+
+            else:
+
+                max_images_in_row = 10
+                new_image_box = self.cs_size * max_images_in_row
+                additional_rows = self.cs_size * ceil(int(self.num_classes)/max_images_in_row)
+                new_image_height = self.cs_size * max_images_in_row + additional_rows
+
+                modified = self.add_cheatsheet_rows(
+                    img=img,
+                    sheet=sheet,
+                    max_images_in_row=max_images_in_row,
+                    new_image_box=new_image_box,
+                    new_image_height=new_image_height,
+                    additional_rows=additional_rows
+                )
         else:
             modified = img.resize((new_image_box, new_image_height))
             
